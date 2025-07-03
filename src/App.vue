1b2a3f49833b2002b88e1250352b85454beb1f49
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-07-03 10:30:15 +0200

    1

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,34 @@
   <router-view />
 </template>
 
-<script setup>
-import { onMounted } from 'vue';
-import { useStore } from 'vuex';
-import { useRouter } from 'vue-router';
-import VuexORM from '@vuex-orm/core';
-import VuexORMAxios from '@vuex-orm/plugin-axios';
-import axios from 'axios';
-import { generateRouteLineages } from 'src/store/modules/routeLineages';
+<script>
+import { onMounted } from 'vue'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+import VuexORM from '@vuex-orm/core'
+import VuexORMAxios from '@vuex-orm/plugin-axios'
+import axios from 'axios'
+import { generateRouteLineages } from 'src/store/modules/routeLineages'
 
-defineOptions({
-  name: 'App'
-});
+export default {
+  name: 'App',
 
-onMounted(() => {
-  const store = useStore();
-  const router = useRouter();
+  mounted () {
+    const store = useStore()
+    const router = useRouter()
 
-  // Initialize Vuex ORM with Axios plugin
-  VuexORM.use(VuexORMAxios, {axios, baseURL: 'http://aiv-team-2.0.test/api'});
+    // Initialize Vuex ORM with Axios plugin
+    VuexORM.use(VuexORMAxios, {
+      axios,
+      baseURL: 'http://aiv-team-2.0.test/api'
+    })
 
-
-  // Ensure router and routes are available
-  if (router && router.options && router.options.routes) {
-    generateRouteLineages(router.options.routes);
-  } else {
-    console.error('Router is not properly initialized or routes are not accessible.');
+    // Ensure router and routes are available
+    if (router && router.options && router.options.routes) {
+      generateRouteLineages(router.options.routes)
+    } else {
+      console.error('Router is not properly initialized or routes are not accessible.')
+    }
   }
-});
+}
 </script>
